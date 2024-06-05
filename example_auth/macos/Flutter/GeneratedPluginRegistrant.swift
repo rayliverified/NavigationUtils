12a29d77a677324504a1470b4d31af44b7b5879e
Ray Li <ray.li.verified@gmail.com>
2024-06-04 23:18:25 -0500

    Update Firebase v3.0.0

--- example_auth/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example_auth/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,8 +16,6 @@ import firebase_storage
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -31,6 +29,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
