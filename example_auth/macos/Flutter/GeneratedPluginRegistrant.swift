0782c16d164f024956a56f1ae706f4fbaa1f1d94
rayliverified <ray.li.verified@gmail.com>
2024-06-27 22:08:05 -0500

    Update Firebase Dependencies v3.1.1

--- example_auth/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example_auth/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,8 @@ import firebase_storage
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -29,4 +31,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- example_auth/windows/flutter/generated_plugins.cmake
+++ example_auth/windows/flutter/generated_plugins.cmake
@@ -7,6 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   firebase_storage
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
