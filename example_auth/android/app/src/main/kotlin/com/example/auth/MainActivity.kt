0d22e80416c049bbc146d8f8096c7154dd3f9ffb
rayliverified <19896174+rayliverified@users.noreply.github.com>
2024-08-06 19:08:32 -0500

    Update Example Auth Flutter v3.24.0

--- example_auth/android/app/src/main/kotlin/com/example/auth/MainActivity.kt
+++ example_auth/android/app/src/main/kotlin/com/example/auth/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.auth
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- example_auth/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example_auth/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,7 +16,6 @@ import firebase_storage
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -31,6 +30,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
